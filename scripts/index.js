2973d1ed1ea66b7a8d00fa6d2421c4c9f8ab3389
BananaMan <suvorov.andrey755@gmail.com>
2023-03-07 21:16:13 +0300

    v2

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+let openForm = document.querySelector('.profile__container-edit');
+let info = document.querySelector('.popup');
+let popupClose = document.querySelector('.popup__close');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('#name-input');
+let jobInput = document.querySelector('#info-input');
+let title = document.querySelector('.profile__container-title');
+let subtitle = document.querySelector('.profile__container-subtitle');
+
+function openClick() {
+    info.style = 'opacity: 1; visibility: visible';
+}
+openForm.addEventListener('click', openClick);
+
+function closeClick() {
+    info.style = 'opacity: 0; visibility: hidden';
+}
+popupClose.addEventListener('click', closeClick);
+
+function handleFormSubmit (evt) {
+    evt.preventDefault(); 
+    title.textContent = nameInput.value;
+    subtitle.textContent = jobInput.value;
+    formElement.addEventListener('submit', closeClick);
+}   
+formElement.addEventListener('submit', handleFormSubmit);
+
